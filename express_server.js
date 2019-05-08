338ef26bcb0fd4ee03051a5db43e6bd63993ba8d
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-08 23:08:28 +0000

    add log in

--- express_server.js
+++ express_server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,7 +32,8 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL,
+                       longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
@@ -36,16 +41,31 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls/");
+});
+app.post("/urls/:shortURL/edit", (req, res) => {
+  let shortURL = req.params.shortURL
+  res.redirect("/urls/" + shortURL);
+});
 app.post("/urls/:shortURL/delete", (req, res) => {
   console.log(req.params.shortURL);
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls/");
 });
-
+app.post("/urls/:shortURL/update", (req, res) => {
+  console.log(req.body);
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/");
+});
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  var shortURL = generateRandomString();
-  var longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect("/urls/" + shortURL);
 });
@@ -57,9 +77,9 @@ app.listen(PORT, () => {
 });
 
 function generateRandomString() {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
+   let result           = '';
+   let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   let charactersLength = characters.length;
    for ( var i = 0; i < 6; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
    }
